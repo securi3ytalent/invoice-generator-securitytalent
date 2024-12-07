aaed114fc1f5a70bb5775706afce3f97015f7663
securi3ytalent <devmehedi101@gmail.com>
2024-12-07 16:05:32 +0600

    invoice-generator-securitytalent

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -35,7 +35,7 @@ import { JSONLD, ROOTKEYWORDS } from "@/lib/seo";
 import { BASE_URL, GOOGLE_SC_VERIFICATION, LOCALES } from "@/lib/variables";
 
 export const metadata: Metadata = {
-    title: "Invoify | Free Invoice Generator",
+    title: "SecurityTalent | Free Invoice Generator",
     description:
         "Create invoices effortlessly with Invoify, the free invoice generator. Try it now!",
     icons: [{ rel: "icon", url: Favicon.src }],
